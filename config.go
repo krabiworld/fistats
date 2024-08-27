5263910ed633f2e8ecf7c4a463f33c5b35f7f62c
daniel <danil.ch9@proton.me>
2024-08-27 19:59:47 +0500

    add default config

--- config.go
+++ config.go
@@ -8,7 +8,15 @@ type Config struct {
 	Storage fistorage.Storage
 }
 
+var ConfigDefault = Config{
+	Storage: fistorage.NewMemory(),
+}
+
 func configDefault(config ...Config) Config {
+	if len(config) < 1 {
+		return ConfigDefault
+	}
+
 	cfg := config[0]
 
 	if cfg.Storage == nil {
